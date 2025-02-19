58678f499b7982b15f363c48b995512fe227abc8
Mason <choimusong0529@gmail.com>
2025-02-19 15:50:43 +0900

    config 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // ignoreBuildErrors: process.env.IS_VERCEL_ENV === "true",
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+      },
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
